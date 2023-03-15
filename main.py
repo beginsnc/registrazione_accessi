0c0cc24a54efc2fd815aea4ee0dfca88f0643ea2
Gaetano Lucifora <gaetano.lucifora@gmail.com>
2023-03-15 15:33:53 +0100

    Refactory  e creazione classe

--- main.py
+++ main.py
@@ -1,47 +1,48 @@
 from datetime import datetime
-
+from datetime import datetime
 import openpyxl
 
-# aprire il file excel
-workbook = openpyxl.load_workbook("accessi.xlsx")
+class Accesso:
+    def __init__(self, filename):
+        self.filename = filename
+        self.workbook = openpyxl.load_workbook(filename)
+        self.worksheet = self.workbook.active
 
-# selezionare il foglio di lavoro
-worksheet = workbook.active
+    def record_access(self, societa, nominativo):
+        # Trovare l'ultima riga del foglio di lavoro
+        last_row = self.worksheet.max_row + 1
 
-last_row = worksheet.max_row + 1
+        # Ottenere la data e l'ora attuali
+        data = datetime.today().strftime("%Y-%m-%d")
+        ora = datetime.today().strftime("%H:%M:%S")
 
-# ottenere i dati di input
-data = datetime.today().strftime("%Y-%m-%d")
-ora = datetime.today().strftime("%H:%M:%S")
-nome_societa = input("Inserisci il nome della società: ")
+        # Controllare se l'utente è già presente nel foglio di lavoro per oggi
+        for row in self.worksheet.iter_rows(min_row=2, min_col=1, max_col=5):
+            if row[0].value == data and row[2].value == nominativo:
+                # Se l'utente è già presente, registrare l'uscita
+                for cell in self.worksheet[row[0].row]:
+                    if cell.column == 3:
+                        cell.value = nominativo
+                        self.worksheet.cell(row=cell.row, column=5, value=ora)
+                        self.workbook.save(self.filename)
+                        return "Uscita registrata"
+                break
+        else:
+            # Se l'utente non è presente, registrare l'ingresso
+            self.worksheet.cell(row=last_row, column=1).value = data
+            self.worksheet.cell(row=last_row, column=4).value = ora
+            self.worksheet.cell(row=last_row, column=2).value = societa
+            self.worksheet.cell(row=last_row, column=3).value = nominativo
+            self.workbook.save(self.filename)
+            return "Ingresso registrato"
+
+
+
+# Creare un'istanza della classe Accesso
+accessi = Accesso("accessi.xlsx")
+
+azienda = input("Inserisci l'azienda: ")
 nominativo = input("Inserisci il nominativo: ")
 
-
-def uscita():
-    for row in worksheet.iter_rows(min_row=2, min_col=1, max_col=5):
-        if row[0].value == data and row[2].value == nominativo:
-            # Inserisci il nominativo nella quinta casella della stessa riga
-            for cell in worksheet[row[0].row]:
-                if cell.column == 3:
-                    cell.value = nominativo
-                    worksheet.cell(row=cell.row, column=5, value=ora)
-                    return True
-
-
-def ingresso():
-    # aggiungere i dati all'ultima riga del foglio di lavoro
-    worksheet.cell(row=last_row, column=1).value = data
-    worksheet.cell(row=last_row, column=4).value = ora
-    worksheet.cell(row=last_row, column=2).value = nome_societa
-    worksheet.cell(row=last_row, column=3).value = nominativo
-
-
-if uscita():
-    print(f"Uscita registrata, grazie {nominativo}")
-
-else:
-    ingresso()
-    print(f"Ingresso registrato, grazie {nominativo}")
-
-# Salvataggio delle modifiche al file Excel
-workbook.save("accessi.xlsx")
+risultato = accessi.record_access(azienda, nominativo)
+print(risultato)
